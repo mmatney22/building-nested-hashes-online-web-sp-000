0cd64728fcdbfe9dddd02d5418e034f192a8272b
Morgan Matney <morgan.matney89@gmail.com>
2020-08-02 23:40:05 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,6 +1,11 @@
 def second_challenge
   epic_tragedy = {
-     :montague => {}
+     :montague => {
+       :patriarch => {}
+       :matriarch => {}
+       :hero => {}
+       :hero_friends => []
+     }
      :capulet => {}
   }
 end
